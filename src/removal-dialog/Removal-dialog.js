c14ecfad09eb561ea4c8f1f1377c919b6ba2970b
Vinicius Rocha <mvinicius.gbo@gmail.com>
2019-02-27 15:23:56 -0300

    Style removal confirm dialog

--- src/removal-dialog/Removal-dialog.js
+++ src/removal-dialog/Removal-dialog.js
@@ -3,11 +3,18 @@ import React from 'react';
 import Modal from '../modal/Modal';
 
 const RemovalConfirmDialog = props => (
-	<Modal {...props} header={<h2>Remove tool</h2>}>
+	<Modal {...props} header={
+		<>
+            <img className="Modal__header-icon" src="/close-blue.svg" alt="Removal Icon" />
+			<h2 className="Modal__title">Remove tool</h2>
+		</>
+	}>
 		<p>Are you sure you want to remove <b>{props.tool && props.tool.title}</b>?</p>
 		<div className="Modal__actions">
-			<button onClick={props.close}>Cancel</button>
-			<button onClick={_ => props.remove(props.tool.id)}>Yes, Remove</button>
+			<button className="button-flat"
+				onClick={props.close}>Cancel</button>
+			<button className="button red-bg white-text"
+				onClick={_ => props.remove(props.tool.id)}>Yes, Remove</button>
 		</div>
 	</Modal>
 );
